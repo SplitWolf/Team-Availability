3d030b34dcd69a9d7413ccc1d6c34355613cf476
SplitWolf <mat.jake1360@gmail.com>
2024-05-24 17:07:00 -0400

    feat: Change app component to current app, Update styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DateBarComponent } from './date-bar/date-bar.component';
+import { AvailableTimeEditorComponent} from './available-time-editor/available-time-editor.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "date-bar",
+    component: DateBarComponent
+  },
+  {
+    path: "availabe-time-editor",
+    component: AvailableTimeEditorComponent
+  }
+];
